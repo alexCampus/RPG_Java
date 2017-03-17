2e1042d5190040085989b07ffc04f6ee73b5de02
Alexandre DE PEMBROKE <Alexandre.DEPEMBROKE@Digital-Grenoble.local>
2017-03-17 09:42:35 +0100

    add Nain class

---
+++ src/tp3/Nain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp3;
+
+import tp3.comportement.*;
+
+/**
+ *
+ * @author alexandre.depembroke
+ */
+public class Nain extends Heros{
+    
+    public Nain(){
+        
+    }
+    
+    public Nain(Power esprit, Soin soin, Deplacement dep) {
+        super(esprit, soin, dep);
+  }
+    
+    
+    
+}
